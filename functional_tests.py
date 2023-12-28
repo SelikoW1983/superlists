befc17454544e24fe74e56531063fc9165bafe75
Константин Селиков <selikow@hotmail.com>
2023-12-28 22:23:18 +0300

    ФТ теперь проверяет возможностьввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import  unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -21,19 +23,34 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложеных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # Она набирает в текстовом поле "Купить павлиньи перья" (её хобби -
         # вязание рыболовных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется,  и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
         # Текстовое поле по-прежнему приглашает её добавить ещё один элемент.
         # Она вводит "Сделать мушку из палиньих перьев"
         # (Эдит очень методична)
-
+        
+        self.fail('Закончить тест!')
         # Страница снова обновляется, и теперь показывает оба элемента её списка
 
         # Эдит интересно, запомнит ли сайт её список. Далее она видит, что
@@ -45,4 +62,4 @@ class NewVisitorTest(unittest.TestCase):
         # Удовлетворённая, она снова ложится спать
 
 if __name__ == '__main__':
-      unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
