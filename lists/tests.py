2b714b8489950a44961f9031656ee04d867d140c
Константин Селиков <selikow@hotmail.com>
2024-01-22 22:15:24 +0300

    Корректировка модели

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -12,26 +12,36 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-class ItemModelTest(TestCase):
+
+class ListAndItemModelsTest(TestCase):
     '''тест модели элемента списка'''
 
     def test_saving_and_retrieving_items(self):
         '''тест сохранения и получения элементов списка'''
+        list_ = List()
+        list_.save()
         first_item = Item()
-        first_item.text = 'The first (ever) list item'
+        first_item.text = 'Первый (самый) элемент списка'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
-        second_item.text = 'Item the second'
+        second_item.text = 'Элемент второй'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(first_saved_item.text, 'Первый (самый) элемент списка')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Элемент второй')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -44,8 +54,9 @@ class ListViewTest(TestCase):
     
     def test_displays_all_items(self):
         '''тест: отображаются все элементы списка'''
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-one-of-a-kind-list-in-the-world/')
 
